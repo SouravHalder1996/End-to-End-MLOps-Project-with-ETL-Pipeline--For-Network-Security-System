000600f6dc9d8b78fcbfa71ca91728224d817fea
Sourav Halder <halder.sourav1996@gmail.com>
2025-11-29 23:38:53 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
-from src.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from src.components.data_transformation import DataTransformation
+from src.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from src.entity.config_entity import TrainingPipelineConfig
 from src.exception.exception import NetworkSecurityException
 from src.logging.logger import logging
@@ -20,6 +21,12 @@ if __name__ == "__main__":
         logging.info("Starting data validation")
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info(f"Data validation completed successfully {data_validation_artifact}")
+
+        data_transformation_config = DataTransformationConfig(training_pipeline_config)
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        logging.info("Starting data transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info(f"Data transformation completed successfully {data_transformation_artifact}")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- src/constants/training_pipeline/__init__.py
+++ src/constants/training_pipeline/__init__.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 TARGET_COLUMN: str = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
@@ -22,3 +23,13 @@ DATA_VALIDATION_VALID_DIR: str = "valid"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+PREPROCCESSING_OBJECT_FILE_NAME: str = "preprocessing.pkl"
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform"
+}
